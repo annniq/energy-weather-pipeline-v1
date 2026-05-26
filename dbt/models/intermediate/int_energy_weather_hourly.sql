2f1afa65a64bcf3804f3d0d449c7e106c9cb7f7b
annniq <44232578+annniq@users.noreply.github.com>
2026-05-26 18:35:45 +0300

    Add files via upload
    Updated int

--- dbt/models/intermediate/int_energy_weather_hourly.sql
+++ dbt/models/intermediate/int_energy_weather_hourly.sql
@@ -31,23 +31,55 @@ joined as (
         weather.shortwave_radiation,
         weather.cloud_cover,
         weather.weather_date,
+        
+        -- Kellaaja kategooria
+        case 
+            when extract(hour from prices.timestamp_utc at time zone 'UTC')::int between 0 and 6 then 'Night'
+            when extract(hour from prices.timestamp_utc at time zone 'UTC')::int between 7 and 10 then 'Morning Peak'
+            when extract(hour from prices.timestamp_utc at time zone 'UTC')::int between 11 and 16 then 'Day'
+            when extract(hour from prices.timestamp_utc at time zone 'UTC')::int between 17 and 20 then 'Evening Peak'
+            else 'Evening'
+        end as time_category,
+
+        -- Päevavalguse loogika
         case
             when weather.shortwave_radiation > 0 then true
             else false
         end as is_daylight_hour,
+        
+        -- Temperatuuri kategooria
         case
-            when weather.temperature_2m_c is null then 'unknown'
-            when weather.temperature_2m_c < 0 then 'freezing'
-            when weather.temperature_2m_c < 10 then 'cold'
-            when weather.temperature_2m_c < 20 then 'mild'
-            else 'warm'
-        end as temperature_band,
+            when weather.temperature_2m_c is null then 'N/A'
+            when weather.temperature_2m_c < 0 then 'Freezing'
+            when weather.temperature_2m_c < 10 then 'Cold'
+            when weather.temperature_2m_c < 20 then 'Mild'
+            else 'Warm'
+        end as temperature_category,
+        
+        -- Tuule kategooria 
         case
-            when weather.wind_speed_10m_kmh is null then 'unknown'
-            when weather.wind_speed_10m_kmh < 10 then 'low_wind'
-            when weather.wind_speed_10m_kmh < 25 then 'medium_wind'
-            else 'high_wind'
-        end as wind_band
+            when weather.wind_speed_10m_kmh is null then 'N/A'
+            when weather.wind_speed_10m_kmh < 10 then 'Low Wind'
+            when weather.wind_speed_10m_kmh < 25 then 'Medium Wind'
+            else 'High Wind'
+        end as wind_category,
+        
+        -- Päikesekiirguse kategooria
+        case
+            when weather.shortwave_radiation is null then 'N/A'
+            when weather.shortwave_radiation >= 500 then 'Very Sunny'
+            when weather.shortwave_radiation >= 200 then 'Sunny'
+            else 'Cloudy/Night'
+        end as solar_category,
+        
+        -- Pilvisuse kategooria
+        case 
+            when weather.cloud_cover is null then 'N/A'
+            when weather.cloud_cover < 20 then 'Clear Sky'
+            when weather.cloud_cover between 20 and 80 then 'Partly Cloudy'
+            else 'Overcast'
+        end as cloud_category   
+
     from prices
     inner join weather
         on prices.country_code = weather.country_code
