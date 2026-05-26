77bb6bd6dade5a1bb312cd5ff7c5b3d414140e6c
annniq <44232578+annniq@users.noreply.github.com>
2026-05-26 18:36:41 +0300

    Add files via upload
    updated marts

--- dbt/models/marts/fct_energy_weather_hourly.sql
+++ dbt/models/marts/fct_energy_weather_hourly.sql
@@ -16,10 +16,24 @@ with source_rows as (
         wind_speed_10m_kmh,
         shortwave_radiation,
         cloud_cover,
+        weather_date,
+        time_category,
         is_daylight_hour,
-        temperature_band,
-        wind_band
+        temperature_category,
+        wind_category,
+        solar_category,
+        cloud_category
     from {{ ref('int_energy_weather_hourly') }}
+
+    {% if is_incremental() %}
+    -- Kui on incremental run, võetakse vaatlusse ainult uued read, mida siht-tabelis veel pole
+    where not exists (
+        select 1
+        from {{ this }} as existing_rows
+        where existing_rows.country_code = int_energy_weather_hourly.country_code
+            and existing_rows.timestamp_utc = int_energy_weather_hourly.timestamp_utc
+    )
+    {% endif %}
 )
 
 select
@@ -32,16 +46,11 @@ select
     wind_speed_10m_kmh,
     shortwave_radiation,
     cloud_cover,
+    weather_date,
+    time_category,
     is_daylight_hour,
-    temperature_band,
-    wind_band
+    temperature_category,
+    wind_category,
+    solar_category,
+    cloud_category
 from source_rows
-
-{% if is_incremental() %}
-where not exists (
-    select 1
-    from {{ this }} as existing_rows
-    where existing_rows.country_code = source_rows.country_code
-        and existing_rows.timestamp_utc = source_rows.timestamp_utc
-)
-{% endif %}
