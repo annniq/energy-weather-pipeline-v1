86062e9b0d5abedacca9563516946ce554446ecc
annniq <44232578+annniq@users.noreply.github.com>
2026-05-31 13:01:29 +0300

    Add files via upload

--- dbt/models/marts/mart_energy_weather_by_condition.sql
+++ dbt/models/marts/mart_energy_weather_by_condition.sql
@@ -4,19 +4,34 @@
         unique_key=[
             'country_code',
             'date_utc',
-            'temperature_band',
-            'wind_band',
+            'temperature_category',
+            'wind_category',
             'is_daylight_hour'
         ]
     )
 }}
 
-with condition_summary as (
+with filtered_source as (
+    select *
+    from {{ ref('fct_energy_weather_hourly') }}
+
+    {% if is_incremental() %}
+    -- Võtame alustabelist ainult need kuupäevad, mida sihttabelis veel ei eksisteeri
+    where date_utc not in (
+        select distinct date_utc 
+        from {{ this }}
+    )
+    {% endif %}
+),
+
+condition_summary as (
     select
         country_code,
         date_utc,
-        temperature_band,
-        wind_band,
+        temperature_category,
+        wind_category,
+		solar_category,
+		cloud_category,
         is_daylight_hour,
         count(*) as hourly_observation_count,
         avg(price_eur_mwh) as avg_price_eur_mwh,
@@ -24,20 +39,24 @@ with condition_summary as (
         avg(wind_speed_10m_kmh) as avg_wind_speed_10m_kmh,
         avg(shortwave_radiation) as avg_shortwave_radiation,
         avg(cloud_cover) as avg_cloud_cover
-    from {{ ref('fct_energy_weather_hourly') }}
+    from filtered_source
     group by
         country_code,
         date_utc,
-        temperature_band,
-        wind_band,
+        temperature_category,
+        wind_category,
+		solar_category,
+		cloud_category,
         is_daylight_hour
 )
 
 select
     country_code,
     date_utc,
-    temperature_band,
-    wind_band,
+    temperature_category,
+    wind_category,
+	solar_category,
+	cloud_category,
     is_daylight_hour,
     hourly_observation_count,
     avg_price_eur_mwh,
@@ -46,15 +65,3 @@ select
     avg_shortwave_radiation,
     avg_cloud_cover
 from condition_summary
-
-{% if is_incremental() %}
-where not exists (
-    select 1
-    from {{ this }} as existing_rows
-    where existing_rows.country_code = condition_summary.country_code
-        and existing_rows.date_utc = condition_summary.date_utc
-        and existing_rows.temperature_band = condition_summary.temperature_band
-        and existing_rows.wind_band = condition_summary.wind_band
-        and existing_rows.is_daylight_hour = condition_summary.is_daylight_hour
-)
-{% endif %}
